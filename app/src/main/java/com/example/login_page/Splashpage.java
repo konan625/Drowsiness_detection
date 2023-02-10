a15c706ae820daddfd505912cc7e559b2e1b257c
konan625 <aanchal625121@gmail.com>
2023-02-10 14:13:19 +0530

    1st

--- app/src/main/java/com/example/login_page/Splashpage.java
+++ app/src/main/java/com/example/login_page/Splashpage.java
@@ -2,7 +2,9 @@ package com.example.login_page;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splashpage extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Splashpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashpage);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        },3500);
     }
 }
